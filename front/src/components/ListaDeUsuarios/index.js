ea4fea83ee0d38a654ff2f29685aacdca663282a
Israel Falcão <israel.jesus@ba.estudante.senai.br>
2025-05-20 20:32:50 -0300

    Update index.js

--- front/src/components/ListaDeUsuarios/index.js
+++ front/src/components/ListaDeUsuarios/index.js
@@ -1,143 +1,45 @@
-// src\components\FormularioCadastro\index.js
+// src\components\ListaDeUsuarios\index.js
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import './styles.css'
-import { useNavigate } from "react-router-dom";
-import useMensagem from '../../hooks/useMensagem'
-import MensagemFeedback from '../MensagemFeedback'
-import logo from '../../assets/images/logo.png'
-import axios from 'axios'
 
-function FormularioCadastro() {
-    const [nome, setNome] = useState('')
-    const [sexo, setSexo] = useState('')
-    const [idade, setIdade] = useState('')
-    const [altura, setAltura] = useState('')
-    const [peso, setPeso] = useState('')
-    const [posicao, setPosicao] = useState('')
-    const [numeroDaCamisa, setNumeroDaCamisa] = useState('')
-    const navigate = useNavigate()
-    const { exibirMensagem , mensagem, tipoMensagem, visivel, fecharMensagem } = useMensagem()
+function ListaDeUsuarios() {
+    const [usuarios, setUsuarios] = useState([])
 
-    const cadastrarUsuario = async () => {
-        try {
-            const response = await axios.post('http://localhost:8080/usuarios', {nome, sexo, idade, altura, peso, posicao, numeroDaCamisa})
-            exibirMensagem(response.data.mensagem || 'Usuário cadastrado com sucesso!', 'sucesso')
-            setNome('')
-            setSexo('')
-            setIdade('')
-            setAltura('')
-            setPeso('')
-            setPosicao('')
-            setNumeroDaCamisa('')
-        } catch (error) {
-            let erroMsg = 'Erro ao conectar ao servidor.'
-            if (error.response && error.response.data) {
-                erroMsg = error.response.data.mensagem
-                if (error.response.data.erros) {
-                    erroMsg += ' ' + Object.values(error.response.data.erros).join(', ')
-                }
+    useEffect(() => {
+        const carregarUsuarios = async () => {
+            try {
+                const response = await axios.get('http://localhost:8080/usuarios')
+                setUsuarios(response.data)
+            } catch (error) {
+                alert('Erro ao buscar usuários: ', error)
+                setUsuarios([])
             }
-            exibirMensagem(erroMsg, 'erro')
         }
-    }
+        carregarUsuarios()
+    }, [])
 
     return (
-        <div className="container">
-            <img src={logo} alt="Logo da empresa" />
-            <h2>Cadastro de Jogadores</h2>
-            <form onSubmit={(e) => {e.preventDefault(); cadastrarUsuario()}}>
-                <input 
-                    type="text"
-                    id="nome"
-                    placeholder="Nome"
-                    value={nome}
-                    onChange={(e) => setNome(e.target.value)}
-                    required
-                />
-                <input 
-                    type="number"
-                    id="idade"
-                    placeholder="Idade"
-                    value={idade}
-                    onChange={(e) => setIdade(e.target.value)}
-                    required
-                />
-                <input 
-                    type="text"
-                    id="altura"
-                    placeholder="Altura"
-                    value={altura}
-                    onChange={(e) => setAltura(e.target.value)}
-                    required
-                />
-                <input 
-                    type="text"
-                    id="peso"
-                    placeholder="Peso"
-                    value={peso}
-                    onChange={(e) => setPeso(e.target.value)}
-                    required
-                />
-                <input 
-                    type="text"
-                    id="posicao"
-                    placeholder="Posição"
-                    value={posicao}
-                    onChange={(e) => setPosicao(e.target.value)}
-                    required
-                />
-                  <input 
-                    type="number"
-                    id="numeroDaCamisa"
-                    placeholder="Numero da Camisa"
-                    value={numeroDaCamisa}
-                    onChange={(e) => setNumeroDaCamisa(e.target.value)}
-                    required
-                />
-            <div className="sexo-container">
-                    <label>Sexo</label>
-                    <label>
-                        <input
-                            type="radio"
-                            id="masculino"
-                            name="sexo"
-                            value="MASCULINO"
-                            checked={sexo === "MASCULINO"}
-                            onChange={(e) => setSexo(e.target.value)}
-                            required
-                        />
-                        Masculino
-                    </label>
-                    <label>
-                        <input
-                            type="radio"
-                            id="feminino"
-                            name="sexo"
-                            value="FEMININO"
-                            checked={sexo === "FEMININO"}
-                            onChange={(e) => setSexo(e.target.value)}
-                            required
-                        />
-                        Feminino
-                    </label>
-                </div>
-                
-                <button type="submit">Cadastrar</button>
-            </form>
-
-            <button onClick={() => navigate('/usuarios')} className="link-usuarios">
-                Ver usuários cadastrados
-            </button>
-
-            <MensagemFeedback
-                mensagem={mensagem}
-                tipo={tipoMensagem}
-                visivel={visivel}
-                onclose={fecharMensagem}
-            />
-        </div>
+        <ul id="listaUsurios" className="lista-usuarios">
+            {usuarios.length === 0 ? (
+                <li>Nenhum usuário encontrado.</li>
+            ) : (
+                usuarios.map( usuario => (
+                    <li key={usuario.id}>
+                        <strong>Nome: </strong> {usuario.nome}<br />
+                        <strong>Sexo: </strong> {usuario.sexo}<br />
+                        <strong>Idade: </strong> {usuario.idade}<br />
+                        <strong>Altura: </strong> {usuario.altura}<br />
+                        <strong>Peso: </strong> {usuario.peso}<br />
+                        <strong>Posicão: </strong> {usuario.posicao}<br />
+                        <strong>Numero da Camisa: </strong> {usuario.numeroDaCamisa}       
+                    </li>
+                ))
+            )}
+        </ul>
     )
+    
 }
 
-export default FormularioCadastro
+export default ListaDeUsuarios
